7bec0add83cb98cdf7e3989b56a7f16c457bea50
mateiash <99895798+mateiash@users.noreply.github.com>
2025-09-28 18:05:34 +0300

    Added progress to now playing notification

--- custom-scripts/custom-scripts/nowplaying.sh
+++ custom-scripts/custom-scripts/nowplaying.sh
@@ -7,10 +7,17 @@ art=$(playerctl metadata --format "{{ mpris:artUrl }}")
 position=$(playerctl position --format "{{ duration(position) }}")
 length=$(playerctl metadata --format "{{ duration(mpris:length) }}")
 
+positionmilis=$(playerctl position --format "{{ position }}")
+lengthmilis=$(playerctl metadata --format "{{ mpris:length }}")
+
+#ratio=$(echo "scale=2 ; 100 * $positionmilis / $lengthmilis" | bc)
+ratio=$((100 * $positionmilis / $lengthmilis))
+echo $ratio
+
 art_temp_path="/tmp/nowplaying_art"
 
 wget -q "$art" -O "$art_temp_path"
 
-notify-send --icon="$art_temp_path" "$title" "by $artist on $album\n$position / $length"
+notify-send -h int:value:$ratio --urgency="low" -i "$art_temp_path" "$title" "by $artist on $album\n$position / $length"
 
 rm $art_temp_path
